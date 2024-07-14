f5bbf63065968dd937c0a88fd66b664d47e537d0
Wang Xiaomei <XianYuPigeon@outlook.com>
2024-07-14 23:47:59 +0800

    Update

--- mian.py
+++ mian.py
@@ -1,2 +1,22 @@
 # -*- coding: utf-8 -*-
 
+#引入库
+import requests
+from bs4 import BeautifulSoup
+
+# 基础URL
+base_url = 'https://hayqbhgr.slider.kz/#'
+
+def get_search_url():
+    global url
+    query_string = input('请输入搜索关键词：')
+    url = base_url + query_string
+    return url
+
+# 发送HTTP请求获取网页内容
+get_search_url()
+response = requests.get(url)
+
+
+
+
